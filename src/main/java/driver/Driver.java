d874812af79d3f5efbc602126324d12d0f10c126
Sergei Tsarik <49017393+github4ta@users.noreply.github.com>
2023-01-03 19:39:21 +0300

    Update Driver.java

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -16,8 +16,8 @@ public class Driver {
     public static WebDriver getInstance() {
         if (driver == null) {
             ChromeOptions opt = new ChromeOptions();
-            opt.addArguments("--lang=en-US");
-            opt.addArguments("--accept-lang=en-US");
+            opt.addArguments("--lang=en_US");
+            opt.addArguments("--accept-lang=en_US");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver(opt);
             driver.manage().window().maximize();
